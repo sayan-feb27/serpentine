f257362d1b858e7471a07309366143942db57a12
Sayan B. <sayan.badarchy@gmail.com>
2024-07-18 10:03:33 +0400

    installing pre-commit hooks: isort, black
    formatting project via black

--- serpentine/configs/urls.py
+++ serpentine/configs/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from serpentine.api.urls import api_v1
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', api_v1.urls),
+    path("admin/", admin.site.urls),
+    path("api/v1/", api_v1.urls),
 ]
